de3eda84e5fa35dafe23ba465178bbb0e2b6c560
Etienne Schmitz <hello@etienne-schmitz.com>
2020-12-23 17:50:17 +0100

    resolve some issue about field

--- src/stories/Field.stories.tsx
+++ src/stories/Field.stories.tsx
@@ -1,17 +1,41 @@
 import React from "react";
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Story, Meta } from "@storybook/react/types-6-0";
-import { FieldView, FieldProps } from "./field/FieldView";
+import { FieldView, FieldViewProps } from "./field/FieldView";
 
 export default {
   title: "Example/Field",
   component: FieldView,
 } as Meta;
 
-const Template: Story<FieldProps> = (args) => <FieldView {...args} />;
+const canvasSize = { width: 1080, height: 700 };
+
+const Template: Story<FieldViewProps> = (args) => {
+  return (
+    <div style={canvasSize}>
+      <FieldView {...args} />;
+    </div>
+  );
+};
 
 export const FieldWithoutRobot = Template.bind({});
 
 FieldWithoutRobot.args = {
+  field: { size: { width: 6, length: 9 } },
+  robots: {
+    blue: [],
+    yellow: [],
+  },
+  container: canvasSize,
+};
+
+export const FieldWithRobot = Template.bind({});
+
+FieldWithRobot.args = {
   field: { size : { width : 6, length : 9}},
+  robots: {
+    blue : [],
+    yellow: []
+  },
+  container: canvasSize
 };
